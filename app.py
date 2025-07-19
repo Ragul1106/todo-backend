bad141188982b6edb4604841f843059f8ba4c95d
Ragul116 <ragulranjith1106@gmail.com>
2025-07-19 19:20:56 +0530

    add deploy link from netlify

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import config
 
 app = Flask(__name__)
 
-CORS(app, origins=["https://deluxe-frangipane-1235fb.netlify.app/"], supports_credentials=True)
+CORS(app, origins=["https://deluxe-frangipane-1235fb.netlify.app"], supports_credentials=True)
 
 app.config['MYSQL_HOST'] = config.MYSQL_HOST
 app.config['MYSQL_USER'] = config.MYSQL_USER
